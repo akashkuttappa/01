4446a5a661046bf5b4dd0ea3557e8321cf444385
Davy Peter Braun <543614+dheavy@users.noreply.github.com>
2024-04-01 12:46:02 +0200

    Add updated list of linted files

--- software/source/clients/base_device.py
+++ software/source/clients/base_device.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+
 load_dotenv()  # take environment variables from .env.
 
 import os
@@ -7,18 +8,13 @@ import asyncio
 import threading
 import sounddevice
 import pyaudio
-from starlette.websockets import WebSocket
-from queue import Queue
 from pynput import keyboard
 import json
 import traceback
 import websockets
 import queue
-import pydub
-import ast
 from pydub import AudioSegment
 from pydub.playback import play
-import io
 import time
 import wave
 import tempfile
@@ -26,7 +22,10 @@ from datetime import datetime
 import cv2
 import base64
 import platform
-from interpreter import interpreter # Just for code execution. Maybe we should let people do from interpreter.computer import run?
+from interpreter import (
+    interpreter,
+)  # Just for code execution. Maybe we should let people do from interpreter.computer import run?
+
 # In the future, I guess kernel watching code should be elsewhere? Somewhere server / client agnostic?
 from ..server.utils.kernel import put_kernel_messages_into_queue
 from ..server.utils.get_system_info import get_system_info
@@ -34,6 +33,7 @@ from ..server.utils.process_utils import kill_process_tree
 
 from ..server.utils.logs import setup_logging
 from ..server.utils.logs import logger
+
 setup_logging()
 
 os.environ["STT_RUNNER"] = "server"
@@ -79,6 +79,7 @@ else:
 p = pyaudio.PyAudio()
 
 send_queue = queue.Queue()
+
 
 class Device:
     def __init__(self):
@@ -126,11 +127,10 @@ class Device:
 
         return image_path
 
-
     def encode_image_to_base64(self, image_path):
         """Encodes an image file to a base64 string."""
         with open(image_path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode('utf-8')
+            return base64.b64encode(image_file.read()).decode("utf-8")
 
     def add_image_to_send_queue(self, image_path):
         """Encodes an image and adds an LMC message to the send queue with the image data."""
@@ -139,7 +139,7 @@ class Device:
             "role": "user",
             "type": "image",
             "format": "base64.png",
-            "content": base64_image
+            "content": base64_image,
         }
         send_queue.put(image_message)
         # Delete the image file from the file system after sending it
@@ -150,7 +150,6 @@ class Device:
         for image_path in self.captured_images:
             self.add_image_to_send_queue(image_path)
         self.captured_images.clear()  # Clear the list after sending
-
 
     async def play_audiosegments(self):
         """Plays them sequentially."""
@@ -166,27 +165,35 @@ class Device:
             except:
                 logger.info(traceback.format_exc())
 
-
     def record_audio(self):
-
-        if os.getenv('STT_RUNNER') == "server":
+        if os.getenv("STT_RUNNER") == "server":
             # STT will happen on the server. we're sending audio.
-            send_queue.put({"role": "user", "type": "audio", "format": "bytes.wav", "start": True})
-        elif os.getenv('STT_RUNNER') == "client":
+            send_queue.put(
+                {"role": "user", "type": "audio", "format": "bytes.wav", "start": True}
+            )
+        elif os.getenv("STT_RUNNER") == "client":
             # STT will happen here, on the client. we're sending text.
             send_queue.put({"role": "user", "type": "message", "start": True})
         else:
             raise Exception("STT_RUNNER must be set to either 'client' or 'server'.")
 
         """Record audio from the microphone and add it to the queue."""
-        stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+        stream = p.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK,
+        )
         print("Recording started...")
         global RECORDING
 
         # Create a temporary WAV file to store the audio data
         temp_dir = tempfile.gettempdir()
-        wav_path = os.path.join(temp_dir, f"audio_{datetime.now().strftime('%Y%m%d%H%M%S%f')}.wav")
-        wav_file = wave.open(wav_path, 'wb')
+        wav_path = os.path.join(
+            temp_dir, f"audio_{datetime.now().strftime('%Y%m%d%H%M%S%f')}.wav"
+        )
+        wav_file = wave.open(wav_path, "wb")
         wav_file.setnchannels(CHANNELS)
         wav_file.setsampwidth(p.get_sample_size(FORMAT))
         wav_file.setframerate(RATE)
@@ -203,17 +210,30 @@ class Device:
         duration = wav_file.getnframes() / RATE
         if duration < 0.3:
             # Just pressed it. Send stop message
-            if os.getenv('STT_RUNNER') == "client":
+            if os.getenv("STT_RUNNER") == "client":
                 send_queue.put({"role": "user", "type": "message", "content": "stop"})
                 send_queue.put({"role": "user", "type": "message", "end": True})
             else:
-                send_queue.put({"role": "user", "type": "audio", "format": "bytes.wav", "content": ""})
-                send_queue.put({"role": "user", "type": "audio", "format": "bytes.wav", "end": True})
+                send_queue.put(
+                    {
+                        "role": "user",
+                        "type": "audio",
+                        "format": "bytes.wav",
+                        "content": "",
+                    }
+                )
+                send_queue.put(
+                    {
+                        "role": "user",
+                        "type": "audio",
+                        "format": "bytes.wav",
+                        "end": True,
+                    }
+                )
         else:
             self.queue_all_captured_images()
 
-            if os.getenv('STT_RUNNER') == "client":
-
+            if os.getenv("STT_RUNNER") == "client":
                 # THIS DOES NOT WORK. We moved to this very cool stt_service, llm_service
                 # way of doing things. stt_wav is not a thing anymore. Needs work to work
 
@@ -224,12 +244,19 @@ class Device:
                 send_queue.put({"role": "user", "type": "message", "end": True})
             else:
                 # Stream audio
-                with open(wav_path, 'rb') as audio_file:
+                with open(wav_path, "rb") as audio_file:
                     byte_data = audio_file.read(CHUNK)
                     while byte_data:
                         send_queue.put(byte_data)
                         byte_data = audio_file.read(CHUNK)
-                send_queue.put({"role": "user", "type": "audio", "format": "bytes.wav", "end": True})
+                send_queue.put(
+                    {
+                        "role": "user",
+                        "type": "audio",
+                        "format": "bytes.wav",
+                        "end": True,
+                    }
+                )
 
         if os.path.exists(wav_path):
             os.remove(wav_path)
@@ -252,24 +279,27 @@ class Device:
 
         if keyboard.Key.space in self.pressed_keys:
             self.toggle_recording(True)
-        elif {keyboard.Key.ctrl, keyboard.KeyCode.from_char('c')} <= self.pressed_keys:
+        elif {keyboard.Key.ctrl, keyboard.KeyCode.from_char("c")} <= self.pressed_keys:
             logger.info("Ctrl+C pressed. Exiting...")
             kill_process_tree()
             os._exit(0)
 
     def on_release(self, key):
         """Detect spacebar release and 'c' key press for camera, and handle key release."""
-        self.pressed_keys.discard(key)  # Remove the released key from the key press tracking set
+        self.pressed_keys.discard(
+            key
+        )  # Remove the released key from the key press tracking set
 
         if key == keyboard.Key.space:
             self.toggle_recording(False)
-        elif CAMERA_ENABLED and key == keyboard.KeyCode.from_char('c'):
+        elif CAMERA_ENABLED and key == keyboard.KeyCode.from_char("c"):
             self.fetch_image_from_camera()
-
 
     async def message_sender(self, websocket):
         while True:
-            message = await asyncio.get_event_loop().run_in_executor(None, send_queue.get)
+            message = await asyncio.get_event_loop().run_in_executor(
+                None, send_queue.get
+            )
             if isinstance(message, bytes):
                 await websocket.send(message)
             else:
@@ -282,7 +312,9 @@ class Device:
 
         async def exec_ws_communication(websocket):
             if CAMERA_ENABLED:
-                print("\nHold the spacebar to start recording. Press 'c' to capture an image from the camera. Press CTRL-C to exit.")
+                print(
+                    "\nHold the spacebar to start recording. Press 'c' to capture an image from the camera. Press CTRL-C to exit."
+                )
             else:
                 print("\nHold the spacebar to start recording. Press CTRL-C to exit.")
 
@@ -305,7 +337,6 @@ class Device:
                 # At this point, we have our message
 
                 if message["type"] == "audio" and message["format"].startswith("bytes"):
-
                     # Convert bytes to audio file
 
                     audio_bytes = message["content"]
@@ -319,13 +350,13 @@ class Device:
                         # 16,000 Hz frame rate
                         frame_rate=16000,
                         # mono sound
-                        channels=1
+                        channels=1,
                     )
 
                     self.audiosegments.append(audio)
 
                 # Run the code if that's the client's job
-                if os.getenv('CODE_RUNNER') == "client":
+                if os.getenv("CODE_RUNNER") == "client":
                     if message["type"] == "code" and "end" in message:
                         language = message["format"]
                         code = message["content"]
@@ -333,7 +364,7 @@ class Device:
                         send_queue.put(result)
 
         if is_win10():
-            logger.info('Windows 10 detected')
+            logger.info("Windows 10 detected")
             # Workaround for Windows 10 not latching to the websocket server.
             # See https://github.com/OpenInterpreter/01/issues/197
             try:
@@ -354,42 +385,46 @@ class Device:
                         await asyncio.sleep(2)
 
     async def start_async(self):
-            # Configuration for WebSocket
-            WS_URL = f"ws://{self.server_url}"
-            # Start the WebSocket communication
-            asyncio.create_task(self.websocket_communication(WS_URL))
+        # Configuration for WebSocket
+        WS_URL = f"ws://{self.server_url}"
+        # Start the WebSocket communication
+        asyncio.create_task(self.websocket_communication(WS_URL))
 
-            # Start watching the kernel if it's your job to do that
-            if os.getenv('CODE_RUNNER') == "client":
-                asyncio.create_task(put_kernel_messages_into_queue(send_queue))
+        # Start watching the kernel if it's your job to do that
+        if os.getenv("CODE_RUNNER") == "client":
+            asyncio.create_task(put_kernel_messages_into_queue(send_queue))
 
-            asyncio.create_task(self.play_audiosegments())
+        asyncio.create_task(self.play_audiosegments())
 
-            # If Raspberry Pi, add the button listener, otherwise use the spacebar
-            if current_platform.startswith("raspberry-pi"):
-                logger.info("Raspberry Pi detected, using button on GPIO pin 14")
-                # Use GPIO pin 14
-                pindef = ["gpiochip0", "14"] # gpiofind PIN14
-                print("PINDEF", pindef)
+        # If Raspberry Pi, add the button listener, otherwise use the spacebar
+        if current_platform.startswith("raspberry-pi"):
+            logger.info("Raspberry Pi detected, using button on GPIO pin 15")
+            # Use GPIO pin 15
+            pindef = ["gpiochip4", "15"]  # gpiofind PIN15
+            print("PINDEF", pindef)
 
-                # HACK: needs passwordless sudo
-                process = await asyncio.create_subprocess_exec("sudo", "gpiomon", "-brf", *pindef, stdout=asyncio.subprocess.PIPE)
-                while True:
-                    line = await process.stdout.readline()
-                    if line:
-                        line = line.decode().strip()
-                        if "FALLING" in line:
-                            self.toggle_recording(True)
-                        elif "RISING" in line:
-                            self.toggle_recording(False)
-                    else:
-                        break
-            else:
-                # Keyboard listener for spacebar press/release
-                listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
-                listener.start()
+            # HACK: needs passwordless sudo
+            process = await asyncio.create_subprocess_exec(
+                "sudo", "gpiomon", "-brf", *pindef, stdout=asyncio.subprocess.PIPE
+            )
+            while True:
+                line = await process.stdout.readline()
+                if line:
+                    line = line.decode().strip()
+                    if "FALLING" in line:
+                        self.toggle_recording(False)
+                    elif "RISING" in line:
+                        self.toggle_recording(True)
+                else:
+                    break
+        else:
+            # Keyboard listener for spacebar press/release
+            listener = keyboard.Listener(
+                on_press=self.on_press, on_release=self.on_release
+            )
+            listener.start()
 
     def start(self):
-        if os.getenv('TEACH_MODE') != "True":
+        if os.getenv("TEACH_MODE") != "True":
             asyncio.run(self.start_async())
             p.terminate()
